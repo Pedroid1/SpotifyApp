cd10b332df586c9ddf0229bd2cae9e509495f276
Pedro Henrique Souza Araujo <pedro.steam2016@hotmail.com>
2025-06-03 06:33:45 -0300

    chore: config build.gradle app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,17 @@
+import com.pedroid.convention.ProjectBuildType
+
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.project.android.application)
+    alias(libs.plugins.project.android.hilt)
+    alias(libs.plugins.project.android.navigation)
+    id("com.google.devtools.ksp")
 }
 
 android {
     namespace = "com.pedroid.spotifyapp"
-    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.pedroid.spotifyapp"
-        minSdk = 24
-        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -18,20 +19,23 @@ android {
     }
 
     buildTypes {
-        release {
-            isMinifyEnabled = false
+        val debug by getting {
+            applicationIdSuffix = ProjectBuildType.DEBUG.applicationIdSuffix
+        }
+        val release by getting {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            applicationIdSuffix = ProjectBuildType.RELEASE.applicationIdSuffix
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
+
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
     }
 }
 
@@ -44,15 +48,9 @@ dependencies {
         ":app"
     )
     rootProject.subprojects.forEach { module ->
-        if(module.path !in listExcludes) implementation(project(module.path))
+        if (module.path !in listExcludes) implementation(project(module.path))
     }
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
+    implementation (libs.androidx.core.splashscreen)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
