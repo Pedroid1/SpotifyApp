7af344f23119471a3fee0946da28622b27a94942
Pedro Henrique Souza Araujo <pedro.steam2016@hotmail.com>
2025-06-05 15:00:31 -0300

    feat(albums) fragment xml and adapter for album

---
+++ feature/albums/src/main/java/com/pedroid/albums/AlbumsFragment.kt
@@ -0,0 +1,17 @@
+package com.pedroid.albums
+
+import com.pedroid.common.BaseFragment
+import com.pedroid.feature.albums.R
+import com.pedroid.feature.albums.databinding.FragmentAlbumsBinding
+
+
+class AlbumsFragment : BaseFragment<FragmentAlbumsBinding>(R.layout.fragment_albums) {
+
+    override fun initialWork() {
+
+    }
+
+    override fun setupObservers() {
+
+    }
+}
